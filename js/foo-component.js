cb5b57a13cd87fb9a270676b51cd6a72e807b2a9
kaos1000 <sagrocott@gmail.com>
2018-02-22 11:19:29 -0800

    final for demo

--- js/foo-component.js
+++ js/foo-component.js
@@ -21,10 +21,25 @@ AFRAME.registerComponent('hello-world', {
     // console.log(sceneEl);
     /* */ 
     var scene = document.querySelector('a-scene');
-    var facedown;
+    var facedown, single_card;
      for (var i = 0; i < 4; i++) {
+      facedown = "";
       facedown = document.createElement('a-plane');
       //facedown.setAttribute('color', '#FF9500');
+      /*
+      single_card = eval(facedown);
+      single_card.addEventListener('mouseenter', function (event) {
+        console.log('enter!!!!', event);
+        single_card.setAttribute('color', '#FF9500');
+       
+       
+      });
+      single_card.addEventListener('mouseleave', function (event) {
+        console.log('exit!!!!', event);
+        single_card.setAttribute('color', false);
+       
+      });
+      */
       facedown.setAttribute('height', '.5');
       facedown.setAttribute('width', '.25');
       facedown.setAttribute('material', 'side: double;src:#card_back_texture; repeat: 1 1');
@@ -35,6 +50,57 @@ AFRAME.registerComponent('hello-world', {
       
       scene.appendChild(facedown);
       }
+      var the_color = '#FF9500';
+      var first_card = document.querySelector('#card_0');
+      first_card.addEventListener('mouseenter', function (event) {
+        //console.log('enter!!!!', event);
+        first_card.setAttribute('color', the_color);
+       
+       
+      });
+      first_card.addEventListener('mouseleave', function (event) {
+        //console.log('enter!!!!', event);
+        first_card.setAttribute('color', false);
+       
+      });
+      var second_card = document.querySelector('#card_1');
+      second_card.addEventListener('mouseenter', function (event) {
+        //console.log('enter!!!!', event);
+        second_card.setAttribute('color', the_color);
+       
+       
+      });
+      second_card.addEventListener('mouseleave', function (event) {
+       // console.log('enter!!!!', event);
+        second_card.setAttribute('color', false);
+       
+      });
+      var third_card = document.querySelector('#card_2');
+      third_card.addEventListener('mouseenter', function (event) {
+       // console.log('enter!!!!', event);
+        third_card.setAttribute('color', the_color);
+       
+       
+      });
+      third_card.addEventListener('mouseleave', function (event) {
+        //console.log('enter!!!!', event);
+        third_card.setAttribute('color', false);
+       
+      });  
+      
+      var fourth_card = document.querySelector('#card_3');
+      fourth_card.addEventListener('mouseenter', function (event) {
+        //console.log('enter!!!!', event);
+        fourth_card.setAttribute('color', the_color);
+       
+       
+      });
+      fourth_card.addEventListener('mouseleave', function (event) {
+        //console.log('enter!!!!', event);
+        fourth_card.setAttribute('color', false);
+       
+      });     
+
 
     }
   
